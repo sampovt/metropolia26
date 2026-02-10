039d6631eff0a357e4f8839b7e6242688004aef4
Sampovt <sampontili@gmail.com>
2026-02-10 23:06:46 +0200

    Tehtävät 2

---
+++ Viikko2/Task2.6.py
@@ -0,0 +1,12 @@
+print("Koodi #1")
+
+import random
+numerot = [random.randint(0, 9)for _ in range(3)]
+print(numerot)
+
+print("Koodi #2")
+
+import random
+numerott = [random.randint(1, 6)for _ in range(4)]
+print(numerott)
+
